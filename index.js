40837a31f83e04b2d2c067649e0bb748b5b96b37
Konstantin <goodluckhf@yandex.ru>
2017-05-10 00:28:51 +0300

    создание тестового роута

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 const express = require('express');
+const routes = require('./routes/routes.js');
 const app = express();
+
+
+app.use('/', routes);
+app.use(express.static('public'));
+
+app.listen(3000);
--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', function(req, res) {
+    res.send('start page');
+});
+
+module.exports = router
+
